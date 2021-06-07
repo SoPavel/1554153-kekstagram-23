060ea4a462e5c22cdf6654aebb9da7191d7d9f43
Pavel S <pavellets1490@gmail.com>
2021-06-07 13:43:56 +0200

    module2-task1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 // #1
 function myNumber(min, max) {
-    const randNumber = Math.random() * (max-min);
-    randNumber = Math.round(randNumber);
-    return randNumber;
+  let randNumber = Math.random() * (max-min);
+  randNumber = Math.round(randNumber);
+  return randNumber;
 }
 myNumber(3, 10);
 
 // #2
 function getCeilNumber(min, max) {
-    return Math.ceil(Math.random()* (max - min) - min);
-  }
-  getCeilNumber(10, 20);
+  return Math.ceil(Math.random()* (max - min) - min);
+}
+getCeilNumber(10, 20);
 
 //   Оба примера выдуманны мной
