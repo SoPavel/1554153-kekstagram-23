2502627a2b4cc91cc954845cf413be26cd2efceb
Pavel S <pavellets1490@gmail.com>
2021-06-16 17:40:18 +0200

    Мелкие доработки

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 // #1 https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+  if(min < 0 || max < 0){
+    return false;
+  } else{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 }
-getRandomInt(100, 130);
+
+getRandomInt(200, 130);
 
 
 // #2 Исправил!
-function checkLength (minString, maxString) {
-  return (minString.length <= maxString);
+function checkLength(string, maxLength) {
+  return (string.length <= maxLength);
 }
 
 checkLength();
